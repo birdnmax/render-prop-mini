e92dcc657c5cc6478151faff33abbf2547c5a5f0
Brittany Hancock <bhancock@utah.gov>
2019-07-14 16:28:23 -0600

    completed

---
+++ src/components/LoginForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Form from './Form';
+import { format } from 'path';
+
+export default function LoginForm(props){
+    return(
+        <Form render={form => {
+            return(
+                <div>
+                    <h1>Login</h1>
+                    <input type='text' name='email' placeholder='email' onChange={form.handleChange}/>
+                    <input type='text' name='password' placeholder='password' onChange={form.handleChange}/>
+                    <button onClick={form.handleSubmit}>Submit</button>
+                </div>
+            )
+        }}/>
+    )
+}
